ec0a36520dc5edfecc53c0e857c028d141c7f8c5
Ngoc Dat Nguyen <ngocdat99hl@gmail.com>
2021-06-10 23:07:55 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var swiper = new Swiper('.box[data-layout="43"] .mySwiper', {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper('.box[data-layout="45"] .box-swiper', {
+    spaceBetween: 30,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper('.box[data-layout="53"] .box-swiper', {
+    spaceBetween: 12,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper('.box[data-layout="57"] .box-swiper', {
+    spaceBetween: 16,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
